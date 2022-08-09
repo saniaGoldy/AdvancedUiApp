b54357bc448000dc56575549fc8edc926ff433ec
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-08-09 15:55:25 +0300

    WebView implemented

--- app/src/main/java/com/example/advanceduiapp/WebPageActivity.kt
+++ app/src/main/java/com/example/advanceduiapp/WebPageActivity.kt
@@ -2,10 +2,14 @@ package com.example.advanceduiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class WebPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_web_page)
+        val webView = WebView(this)
+        setContentView(webView)
+        supportActionBar?.title = getString(R.string.menu_web_page)
+        webView.loadUrl("https://developer.android.com/")
     }
 }
