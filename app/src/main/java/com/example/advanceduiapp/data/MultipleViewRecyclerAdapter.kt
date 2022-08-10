38f621b5ae261bb726b60bd56d3336dcceb6954a
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-08-11 02:21:31 +0300

    Multiple View Type RecyclerView implemented

---
+++ app/src/main/java/com/example/advanceduiapp/data/MultipleViewRecyclerAdapter.kt
@@ -0,0 +1,71 @@
+package com.example.advanceduiapp.data
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
+import com.example.advanceduiapp.databinding.ButtonItemBinding
+import com.example.advanceduiapp.databinding.FragmentPagerBinding
+import com.example.advanceduiapp.databinding.TvItemBinding
+
+class MultipleViewRecyclerAdapter(
+    private val pageData: List<Pair<String, Int>>,
+    private val textData: List<String>,
+    private val buttonActionsData: List<() -> Unit>
+) : Adapter<ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return when(viewType){
+            0 -> PageViewHolder(FragmentPagerBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            1 -> TVViewHolder(TvItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            else -> ButtonViewHolder(ButtonItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when(position){
+            in 0..pageData.lastIndex -> 0
+            in pageData.size..(pageData.size+textData.lastIndex) -> 1
+            else -> 2
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return pageData.size + textData.size + buttonActionsData.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when(holder.itemViewType){
+            0 -> (holder as PageViewHolder).bind(pageData[position])
+            1 -> (holder as TVViewHolder).bind(textData[position - pageData.size])
+            else -> (holder as ButtonViewHolder).bind { buttonActionsData[position - pageData.size - textData.size] }
+        }
+    }
+
+    inner class PageViewHolder(
+        binding: FragmentPagerBinding
+    ) : ViewHolder(binding.root) {
+        private val itemTitle = binding.textSlideshow
+
+        private val itemImage = binding.pagerImageView
+        fun bind(item: Pair<String, Int>) {
+            itemTitle.text = item.first
+            itemImage.setImageResource(item.second)
+        }
+    }
+
+    inner class TVViewHolder(binding: TvItemBinding): ViewHolder(binding.root){
+        private val itemTV = binding.itemTV
+        fun bind(item: String){
+            itemTV.text = item
+        }
+    }
+
+    inner class ButtonViewHolder(binding: ButtonItemBinding): ViewHolder(binding.root){
+        private val itemButton = binding.itemButton
+        fun bind(item: ()->Unit){
+            itemButton.setOnClickListener { item.invoke() }
+        }
+    }
+}
--- app/src/main/java/com/example/advanceduiapp/ui/MultipleViewRecyclerFragment.kt
+++ app/src/main/java/com/example/advanceduiapp/ui/MultipleViewRecyclerFragment.kt
@@ -1,60 +1,50 @@
 package com.example.advanceduiapp.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.advanceduiapp.PagerActivity
 import com.example.advanceduiapp.R
+import com.example.advanceduiapp.data.MultipleViewRecyclerAdapter
+import com.example.advanceduiapp.databinding.FragmentMultipleViewRecyclerBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MultipleViewRecyclerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MultipleViewRecyclerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val textData = listOf<String>("some string", "Oi cunt", "wakee wakee", "mornin'")
+    private val pageData = PagerActivity.imageMap
+    private val buttonActions = listOf<() -> Unit>({
+        Toast.makeText(
+            this.requireContext(),
+            getString(R.string.butcher_greeting),
+            Toast.LENGTH_SHORT
+        ).show()
+    },
+        {
+            Toast.makeText(
+                this.requireContext(),
+                getString(R.string.homelander_phrase),
+                Toast.LENGTH_SHORT
+            ).show()
         }
-    }
+    )
+
+    private var _binding: FragmentMultipleViewRecyclerBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_multiple_view_recycler, container, false)
+        _binding = FragmentMultipleViewRecyclerBinding.inflate(inflater, container, false)
+        val root = binding.root
+
+        binding.multipleViewTypeList.adapter =
+            MultipleViewRecyclerAdapter(pageData, textData, buttonActions)
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MultipleViewRecyclerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MultipleViewRecyclerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
