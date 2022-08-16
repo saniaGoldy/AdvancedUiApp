826e4a04a6e465217fd56c12b461d530e10fcf91
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-08-16 23:43:35 +0300

    moved retrieving button data in viewModel

--- app/src/main/java/com/example/advanceduiapp/ui/multiViewRecycler/MultipleViewRecyclerFragment.kt
+++ app/src/main/java/com/example/advanceduiapp/ui/multiViewRecycler/MultipleViewRecyclerFragment.kt
@@ -6,10 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.advanceduiapp.R
 import com.example.advanceduiapp.adapters.MultipleViewRecyclerAdapter
 import com.example.advanceduiapp.data.ItemData
-import com.example.advanceduiapp.data.ToastButtonData
 import com.example.advanceduiapp.databinding.FragmentMultipleViewRecyclerBinding
 
 class MultipleViewRecyclerFragment : Fragment() {
@@ -21,15 +19,11 @@ class MultipleViewRecyclerFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMultipleViewRecyclerBinding.inflate(inflater, container, false)
         val root = binding.root
         viewModel.itemsData.observe(this.viewLifecycleOwner){ itemsData: List<ItemData> ->
-            viewModel.toastButtonData.observe(this.viewLifecycleOwner){ toastStringResources: List<Int> ->
-                val buttonItemsData = toastStringResources.map { ToastButtonData(this.requireContext(), getString(it)) }
-                binding.multipleViewTypeList.adapter =
-                    MultipleViewRecyclerAdapter(itemsData + buttonItemsData)
-            }
+            binding.multipleViewTypeList.adapter = MultipleViewRecyclerAdapter(itemsData)
         }
         return root
     }
--- app/src/main/java/com/example/advanceduiapp/ui/multiViewRecycler/MultipleViewRecyclerViewModel.kt
+++ app/src/main/java/com/example/advanceduiapp/ui/multiViewRecycler/MultipleViewRecyclerViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.advanceduiapp.ui.multiViewRecycler
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.advanceduiapp.R
 import com.example.advanceduiapp.data.ItemData
 import com.example.advanceduiapp.data.PageData
 import com.example.advanceduiapp.data.TextData
+import com.example.advanceduiapp.data.ToastButtonData
 
-class MultipleViewRecyclerViewModel: ViewModel() {
+class MultipleViewRecyclerViewModel(application: Application): AndroidViewModel(application) {
+    private val appContext get() = getApplication<Application>().applicationContext
     private val _itemsData = MutableLiveData(
         listOf(
             PageData(
@@ -15,12 +18,11 @@ class MultipleViewRecyclerViewModel: ViewModel() {
                 R.drawable.harley_quinn
             ),
             TextData("Oi cunt"),
-
+            ToastButtonData(appContext, appContext.getString(R.string.butcher_greeting)),
             TextData("wakee wakee"),
-
+            ToastButtonData(appContext, appContext.getString(R.string.homelander_phrase))
             )
     )
     val itemsData: MutableLiveData<List<ItemData>>
         get() = _itemsData
-    val toastButtonData = MutableLiveData(listOf(R.string.butcher_greeting, R.string.homelander_phrase))
 }
