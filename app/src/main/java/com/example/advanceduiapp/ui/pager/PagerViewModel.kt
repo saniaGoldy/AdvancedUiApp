b949a4e8810707fae161da8e0290ddd8b87a6d65
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-08-20 11:54:12 +0300

    string resources

--- app/src/main/java/com/example/advanceduiapp/ui/pager/PagerViewModel.kt
+++ app/src/main/java/com/example/advanceduiapp/ui/pager/PagerViewModel.kt
@@ -1,20 +1,22 @@
 package com.example.advanceduiapp.ui.pager
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.advanceduiapp.R
 import com.example.advanceduiapp.data.PageData
 
-class PagerViewModel: ViewModel() {
+class PagerViewModel(application: Application): AndroidViewModel(application) {
     private val _showMultipleViewRecyclerFragment = MutableLiveData<Boolean>(false)
     val showMultipleViewRecyclerFragment: LiveData<Boolean> get() = _showMultipleViewRecyclerFragment
 
     val imageMap = MutableLiveData(
         listOf(
-            PageData("CyberCiri", R.drawable.cybercyri),
-            PageData("Geralt and Ciri", R.drawable.the_witcher_3_wild_hunt_wallpaper),
-            PageData("Harley Qeen", R.drawable.harley_quinn)
+            PageData(application.getString(R.string.ciryImageTitle), R.drawable.cybercyri),
+            PageData(application.getString(R.string.Geralt_and_Ciri_Title), R.drawable.the_witcher_3_wild_hunt_wallpaper),
+            PageData(application.getString(R.string.Harley_Quinn), R.drawable.harley_quinn)
         )
     )
 
